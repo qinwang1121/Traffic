0c1de2e3c05efc50023482f48c28f5ef0a461e5c
qinwang <haoyanwang1121@gmail.com>
2021-02-05 03:18:00 +0800

    添加用户数据模型

---
+++ app/src/main/java/com/qinwang/traffic/tools/bean/User.java
@@ -0,0 +1,82 @@
+package com.qinwang.traffic.tools.bean;
+
+/**
+ * @Auther:haoyanwang1121@gmail.com
+ * @Date:2021/1/28
+ * @Description:com.ningya.intelligent_transportation_equipment.tools.bean
+ * @Version:1.0
+ * @function:
+ */
+public class User {
+
+    private String userName;
+
+    private String passWord;
+
+    private String gender;
+
+    private String ID;
+
+    private String phone;
+
+    private String home;
+
+    private String photo;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+}
