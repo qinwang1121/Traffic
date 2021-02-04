664fdd46f917654101350c1acc628aebbd1d905a
qinwang <haoyanwang1121@gmail.com>
2021-02-05 01:38:35 +0800

    添加Login界面

---
+++ app/src/main/java/com/qinwang/traffic/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.qinwang.traffic.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.qinwang.traffic.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
